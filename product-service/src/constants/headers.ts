488f00cfe7887a2f45c89a73c18f82cb313616da
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2021-08-28 17:16:19 +0300

    fix: headers

--- product-service/src/constants/headers.ts
+++ product-service/src/constants/headers.ts
@@ -1,6 +1,7 @@
 export const DEFAULT_HEADERS = {
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': '*',
+    // 'Access-Control-Allow-Methods': '*',
+    // 'Access-Control-Allow-Headers': '*',
+    // 'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
+    'Access-Control-Allow-Credentials': true
 };
