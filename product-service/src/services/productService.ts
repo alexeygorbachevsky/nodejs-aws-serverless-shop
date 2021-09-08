19e5172cdb294688fc1c88e290cc5def5c05cc35
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2021-09-08 21:07:33 +0300

    refactor: createProduct

--- product-service/src/services/productService.ts
+++ product-service/src/services/productService.ts
@@ -17,16 +17,7 @@ class ProductService implements ProductServiceInterface {
 
     async createProduct(product: Omit<ProductInterface, "id">) {
         const {title, price, description, count} = product;
-
-
-        // const query = `
-        // BEGIN;
-        // INSERT INTO product (title, price, description) VALUES ('${title}', ${price}, '${description}')  RETURNING id;
-        // INSERT INTO stock (product_id, count)
-        //     VALUES (id, ${count})  RETURNING *;
-        // COMMIT;
-        // END;
-        // `
+        
         const query = `
         BEGIN;
         WITH insertedProduct AS (
@@ -45,39 +36,8 @@ class ProductService implements ProductServiceInterface {
 
         const result = await invoke(query);
 
-
-        let createdProduct;
-        if (Array.isArray(result)) {
-            createdProduct = result[1]?.rows[0];
-        }
-
-        return createdProduct || null;
+        return Array.isArray(result) ? result[1]?.rows[0] : null;
     }
-
-    // async createProduct(product: Omit<ProductInterface, "id">) {
-    //     const {title, price, description, count} = product;
-    //
-    //     const tempResult = await invoke(`
-    //     INSERT INTO product (title, price, description)
-    //     VALUES ('${title}', ${price}, '${description}')  RETURNING *`);
-    //
-    //     const createdProduct = tempResult?.rows[0]
-    //
-    //     if (!createdProduct) {
-    //         return null;
-    //     }
-    //
-    //     const result = await invoke(`
-    //     INSERT INTO stock (product_id, count)
-    //     VALUES ('${createdProduct.id}', ${count}) RETURNING *;
-    //     `);
-    //     const createdStock = result?.rows[0]
-    //
-    //     if (!createdStock) {
-    //         return null;
-    //     }
-    //     return {...createdProduct, count: createdStock.count};
-    // }
 }
 
 export {ProductService};
